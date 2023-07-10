5fcee49afe672eecd3c14d744484b560c995008a
EmeraldHedgehog <50961967+Meleemaru2k@users.noreply.github.com>
2023-07-10 08:19:23 +0200

    Feature/10 12 updateloop eventhandling (#14)
    * add basic gameloop and event handling
    
    * Change lastUpdate to be a bit more general
    
    * sickness implemented
    
    * rework sicknesses
    
    * comments

--- tamagi/src/components/test.tsx
+++ tamagi/src/components/test.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { TamagiMinMax, useTamagi } from "@/stores/tamagi";
+import gameloop from "@/utils/gameloop";
+import { cpuEvent, userEvent, userEvents } from "@/utils/types";
 import { useDisplay } from "@/stores/display";
-import { /*TamagiMinMax, */ useTamagi } from "@/stores/tamagi";
 import { useTamagiShell } from "@/stores/tamagiShell";
 
 export default function Test() {
@@ -12,12 +14,10 @@ export default function Test() {
 
   //Tamagi
   const setTamagiName = useTamagi((state) => state.setName);
-  const tamagiHunger = useTamagi((state) => state.tamagi.hunger);
-  const setHunger = useTamagi((state) => state.setHunger);
-  const increaseHunger = useTamagi((state) => state.increaseHunger);
-  const decreaseHunger = useTamagi((state) => state.decreaseHunger);
-  const iconIndex = useDisplay((state) => state.display.pageIndex);
+  const tamagi = useTamagi();
+  tamagi.eventInProgress;
 
+  const iconIndex = useDisplay((state) => state.display.pageIndex);
   let tamagiName = "";
 
   const handleSubmit = () => {
@@ -51,9 +51,45 @@ export default function Test() {
       <div className="border-[1px]" onClick={() => tamagiShellColour("teal")}>
         Set Shell Colour To Teal
       </div>
-      <div onClick={() => setHunger(50)}>Hunger to 50</div>
-      <div onClick={() => increaseHunger(2)}>Hunger Increase 2</div>
-      <div onClick={() => decreaseHunger(2)}>Hunger Decrease 2</div>
+
+      <div className="flex flex-col gap-2">
+        <div
+          className="p-2 bg-slate-200"
+          onClick={() => gameloop.addUserEvent(userEvents.feed)}
+        >
+          Dispatch Feed Event
+        </div>
+        <div
+          className="p-2 bg-slate-200"
+          onClick={() => gameloop.addUserEvent(userEvents.healSick)}
+        >
+          Dispatch Heal Event
+        </div>
+
+        <div className="flex flex-col [&_>div:nth-child(2n)]:pb-2 [&_>div:nth-child(2n)]:border-b-2 [&_>div:nth-child(2n)]:border-solid [&_>div:nth-child(2n)]:border-black">
+          <div>Hunger</div>
+          <div>
+            {tamagi.tamagi.hunger} / {TamagiMinMax.HungerMax}
+          </div>
+          <div>Current Event</div>
+          <div>{tamagi.eventInProgress?.type ?? "none"}</div>
+          <div>Event-Progess:</div>
+          <div>
+            {tamagi.eventInProgress
+              ? `${
+                  tamagi.lastUpdate.time - tamagi.eventInProgress?.timeCreated
+                } / 
+          ${tamagi.eventInProgress?.time.toString()}`
+              : "none"}
+          </div>
+          <div>Is Sick:</div>
+          <div>
+            {tamagi.tamagi.sick?.timeHealed === null
+              ? tamagi.tamagi.sick.type.name
+              : "false"}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
